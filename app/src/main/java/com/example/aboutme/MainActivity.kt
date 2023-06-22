74f8e1cfd59b2e656630c7968a543d6cb0b054d0
Jose Garza <90730468+SVENTRIPIKAL@users.noreply.github.com>
2023-06-22 15:44:06 -0500

    data class (created / implemented)
    -added databinding to build.gradle
    -created MyName data class
    -added layout & data tags
    -views share reference to data class
    -referenced binding on as-needed basis in MainActivity.kt
    -removed excess view objects

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,13 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.aboutme.databinding.ActivityMainBinding
 
+
+// exception tag
 const val TAG = "DISPLAY NICKNAME ERROR:"
 
 class MainActivity : AppCompatActivity() {
@@ -19,53 +17,64 @@ class MainActivity : AppCompatActivity() {
     // create lateinit for Activity Binder
     private lateinit var binding: ActivityMainBinding
 
+    // lateinit for MyName data class
+    // cannot be initialized with resource values
+    // until activity & objects have been inflated
+    private lateinit var nameObject: MyName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // inflate activity layout
+        // inflate activity layout & its objects
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        // reference any layouts/views as objects
-        val activityMainLayout: LinearLayout = binding.root
+        // initialize data class with resource value
+        nameObject = MyName(getString(R.string.name_text))
 
-        val editTextView: EditText = binding.editTextView
-        val nicknamePopupView: TextView = binding.nicknamePopupView
-        val doneButton: Button = binding.doneButton
+        // reference any layouts/views as objects if necessary
+        // manipulate data via as-needed references
+        // update data class via binder with new object data created in activity
+        binding.myName = nameObject
 
         // button onClickListener
-        doneButton.setOnClickListener {
-            displayNickName(doneButton, editTextView, nicknamePopupView)
+        binding.doneButton.setOnClickListener {
+
+            // display nickName
+            displayNickName()
         }
 
         // pass layout view
-        setContentView(activityMainLayout)
+        setContentView(binding.mainLayout)
     }
 
 
     /**
-     * function should remove visibility of Button/editTextView
-     * assign editText.text to nicknamePopupView.text,
-     * reassign visibility back to nicknamePopupView, then
-     * finally hide the keyboard.
+     * function should remove visibility of Button/editTextView,
+     * assign editText.text to nicknamePopupView.text via data class,
+     * reassign visibility back to nicknamePopupView, then finally
+     * hide the keyboard.
      */
-    private fun displayNickName(
-        button: Button,
-        inputText: EditText,
-        nicknamePopupView: TextView
-    ) {
+    private fun displayNickName() {
         try {
-            // transfer editText text
-            nicknamePopupView.text = inputText.editableText
 
-            // hide views
-            button.visibility = View.GONE
-            inputText.visibility = View.GONE
+            // block of code referencing binder
+            binding.apply {
 
-            // assign visibility to popupView
-            nicknamePopupView.visibility = View.VISIBLE
+                // transfer editText.text to popUpView.text via data class object
+                myNickName = nameObject.copy(
+                    myNickName = editTextView.editableText.toString()
+                )
 
+                // remove button & editText visibility
+                doneButton.visibility = View.GONE
+                editTextView.visibility = View.GONE
 
-            // provide feedback for any errors as they occur
+                // assign visibility back to popupView
+                // should now reference data class myNickName value
+                nicknamePopupView.visibility = View.VISIBLE
+            }
+
+            // provide feedback for any errors if applicable
         } catch (e: Exception) {
 
             val message = e.message.toString()
@@ -76,7 +85,7 @@ class MainActivity : AppCompatActivity() {
 
             // hide keyboard
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(button.windowToken, 0)
+            imm.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
         }
     }
 }
---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,9 @@
+package com.example.aboutme
+
+/**
+ * data class to hold view values
+ */
+data class MyName(
+    var myName: String = "",
+    var myNickName: String = ""
+)
